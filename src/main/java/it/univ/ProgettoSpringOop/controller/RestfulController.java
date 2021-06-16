6ceafed59ff997c93823d2a1ecd3a35909cbd274
samuele-it <samuele.diaddezio@gmail.com>
2021-06-16 18:09:10 +0200

    Modifiche al programma

--- src/main/java/it/univ/ProgettoSpringOop/controller/RestfulController.java
+++ src/main/java/it/univ/ProgettoSpringOop/controller/RestfulController.java
@@ -1,133 +1,33 @@
 package it.univ.ProgettoSpringOop.controller;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.opencsv.bean.CsvToBeanBuilder;
-
-import it.univ.ProgettoSpringOop.modelli.DataMod;
-import it.univ.ProgettoSpringOop.modelli.ListaMod;
-import it.univ.ProgettoSpringOop.modelli.MetaMod;
-import it.univ.ProgettoSpringOop.modelli.ReqMod;
-
+import it.univ.ProgettoSpringOop.modelli.Record;
+import it.univ.ProgettoSpringOop.gestioneDati.GestDati;
 @RestController
 public class RestfulController {
-	
+	//Metodi richieste GET
 	@GetMapping("/dati")
-	public ArrayList<DataMod> parsingDati() throws FileNotFoundException {
-		return (ArrayList<DataMod>) ListaMod.getLista();
-	}
-	
+	public ArrayList<Record> parsingDati() throws IOException{
+		GestDati.RDdataset("https://dati.unibo.it/dataset/2b7a6274-7c6b-4338-bb14-a36cedc581c9/resource/5f66c3f5-766d-4c9e-bca7-c3a415d504fc/download/orari_2019.csv", GestDati.getLista1());
+		return (ArrayList<Record>) GestDati.getLista1();//restituisce la lista
+		}
 	@GetMapping("/conta")
-	public int conta(@RequestParam(name="atr", defaultValue="CODICE")String atr){
-		int c=0;
-		boolean flag=true;
-		ListaMod.getLista().clear();
-		for(DataMod dato : ListaMod.getLista()) {
-			for(int i=0; i<=ListaMod.getLista().size(); i++) {
-				if(dato.getCol(atr).equals(ListaMod.getLista().get(i).getCol(atr))) {
-					flag=false;
-					break;
-				}
-			}
-			if(flag==true) {
-				c++;
-			}
+	public int conta(@RequestParam(name="atr", defaultValue="CODICE")String atr, ArrayList<Record> lista){
+		return 1;
 		}
-		return c;
-	}
-	
 	@PostMapping("/stats")
-	public ArrayList<DataMod> stats(@RequestBody String strric) throws JsonParseException, JsonMappingException, IOException {
-		ListaMod.getLista().clear();
-		ObjectMapper mapper = new ObjectMapper();
-		ReqMod req = mapper.readValue(strric, ReqMod.class);
-		boolean flag=false;
-		switch (req.getOp()) {
-		case "$not":
-			for(DataMod dato : ListaMod.getLista()) {
-				if(!dato.getCol(req.getAtr(0)).equals(req.getVal(0))) {
-					ListaMod.getStatlist().add(dato);
-				}
-			}
-			break;
-		case "$in":
-			for(DataMod dato : ListaMod.getLista()) {
-				for(int i=0; i<req.getValAsArr().length; i++) {
-					if(dato.getCol(req.getAtr(0)).equals(req.getVal(i))) {
-						flag=true;//risulta almeno un valore
-					}
-				}
-				if(flag==true) {
-					ListaMod.getStatlist().add(dato);
-				}
-				flag=false;
-			}
-			break;
-		case "$nin":
-			for(DataMod dato : ListaMod.getLista()) {
-				for(int i=0; i<req.getValAsArr().length; i++) {
-					if(dato.getCol(req.getAtr(0)).equals(req.getVal(i))) {
-						flag=true;//il valore risulta
-					}
-					if(flag==true) {
-						break;//il valore risulta, esce dal for e passa al prossimo record
-					}
-				}
-				if(flag==false) {//se è false non risulta in alcuna casella dell'array, quindi aggiunge alla lista
-					ListaMod.getStatlist().add(dato);
-				}
-				flag=false;
-			}
-			break;
-		case "$or":
-			for(DataMod dato : ListaMod.getLista()) {
-				if(dato.getCol(req.getAtr(0)).equals(req.getVal(0)) || dato.getCol(req.getAtr(0)).equals(req.getVal(1))) {
-					flag=true;//almeno una condizione soddisfatta
-				}
-				if(flag==true) {
-					ListaMod.getStatlist().add(dato);
-				}
-				flag=false;
-			}
-			break;
-		case "$and":
-			for(DataMod dato : ListaMod.getLista()) {
-				if(dato.getCol(req.getAtr(0)).equals(req.getVal(0)) && dato.getCol(req.getAtr(1)).equals(req.getVal(1))) {
-					flag=true;//tutte e due le condizoni sono soddisfatte
-				}
-				if(flag==true) {
-					ListaMod.getStatlist().add(dato);
-				}
-				flag=false;
-			}
-			break;
+	public ArrayList<Record> stats(@RequestBody String strric, ArrayList<Record> lista) throws JsonParseException, JsonMappingException, IOException {
+		return null;
 		}
-		return (ArrayList<DataMod>) ListaMod.getStatlist();
-	}
-	
 	@GetMapping("/meta")
-	public ArrayList<MetaMod> getMeta(){
-		ListaMod.getListam().clear();
-		DataMod obj = new DataMod();
-		Class<? extends DataMod> classe = obj.getClass();
-		Field[] campi = classe.getDeclaredFields();//ottiene i campi della classe DataMod mettendoli in un array di tipo Field
-		for(int i=0; i<campi.length; i++) {
-			ListaMod.getListam().add(new MetaMod(campi[i].getName().toString(), campi[i].getName().toString(), campi[i].getType().toString()));
+	public ArrayList<Record> getMeta(ArrayList<Record> lista){
+		return null;
 		}
-		return (ArrayList<MetaMod>) ListaMod.getListam();
 	}
-		
-}
--- src/main/java/it/univ/ProgettoSpringOop/gestioneDati/GestDati.java
+++ src/main/java/it/univ/ProgettoSpringOop/gestioneDati/GestDati.java
@@ -1,31 +1,38 @@
 package it.univ.ProgettoSpringOop.gestioneDati;
-
-
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-
-
-
-
+import java.util.ArrayList;
+import com.opencsv.bean.CsvToBeanBuilder;
+import it.univ.ProgettoSpringOop.modelli.Record;
 public class GestDati {
-	
-	public static void scaricaCsv(String targetUrl) throws IOException {
-		File csvDir = new File("Dataset.csv");
-		if(!csvDir.exists()) {
-			System.setProperty("http.agent", "DownloadCSV");
-			URL url = new URL(targetUrl);
-			ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+	private static ArrayList<Record> lista1;
+	public static ArrayList<Record> getLista1() {
+		return lista1;
+	}
+	public static void setLista1(ArrayList<Record> lista1) {
+		GestDati.lista1 = lista1;
+	}
+	public static void RDdataset(String targetUrl, ArrayList<Record> lista) throws IOException {
+		File csvDir = new File("Dataset.csv");//Variabile per controllare se il dataset esiste
+		if(!csvDir.exists()) {//Esegue solo se il dataset esiste nel disco
+			System.setProperty("http.agent", "DownloadCSV");//Setta lo useragent
+			URL url = new URL(targetUrl);//Url da dove scaricare il csv, passato come argomento dal main
+			ReadableByteChannel rbc = Channels.newChannel(url.openStream());//Apre canale di stream
 			FileOutputStream fos = new FileOutputStream("Dataset.csv");
 			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-			fos.close();
-			System.out.println("Dataset scaricato");
-		}else {
+			fos.close();//Non serve più scrivere su disco
+			//System.out.println("Dataset scaricato");
+			lista.clear();//Evita duplicazione in caso di reiterazione della richiesta
+			FileReader fr = new FileReader("Dataset.csv");//Apre file reader
+			lista.addAll((new CsvToBeanBuilder<Record>(fr).withSeparator(',').withType(Record.class).build().parse()));//Spring parsa il csv
+			fr.close();//Non dovrebbero servire altri inputs
+			}else {
 			System.out.println("Nessun download");
+			}
 		}
-	}
 }
