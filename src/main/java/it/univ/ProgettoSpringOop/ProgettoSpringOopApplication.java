d198e2012969daf03fedcf17c7b5760d41ed8e5c
AndreaBuondi <36244333+PineiF@users.noreply.github.com>
2021-09-16 17:58:28 +0200

    Add files via upload
    Il main è stato aggiornato

--- src/main/java/it/univ/ProgettoSpringOop/ProgettoSpringOopApplication.java
+++ src/main/java/it/univ/ProgettoSpringOop/ProgettoSpringOopApplication.java
@@ -1,9 +1,23 @@
 package it.univ.ProgettoSpringOop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Buondi Andrea & Di Addezio Samuele
+ *
+ * @version 1.2
+ */
 @SpringBootApplication
+
+//il main è vuoto e la sua unica funzione è quella di poter essere avviato come Applicazione SpringBoot
+
 public class ProgettoSpringOopApplication {
+	
 	public static void main(String[] args){
+		
+		
 		SpringApplication.run(ProgettoSpringOopApplication.class, args);
+		
+		
 		}
+	
 	}
